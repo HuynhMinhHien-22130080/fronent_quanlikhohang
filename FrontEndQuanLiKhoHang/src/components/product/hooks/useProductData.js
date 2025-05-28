1d614ea93b089623cb958547302400ba5ba40512
Your Name <22130080@st.hcmuaf.edu.vn>
2025-05-28 12:05:57 +0700

    set time

--- FrontEndQuanLiKhoHang/src/components/product/hooks/useProductData.js
+++ FrontEndQuanLiKhoHang/src/components/product/hooks/useProductData.js
@@ -120,7 +120,7 @@ export const useProductData = () => {
   useEffect(() => {
     fetchData(); 
 
-    const interval = setInterval(fetchData, 30*1000); 
+    const interval = setInterval(fetchData, 3000*1000); 
 
     return () => clearInterval(interval);
   }, []);
